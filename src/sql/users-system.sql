b0004338a05a53df91e8e5575535583dd27bb1f8
Antoine Bergerault <antoine.bergerault@gmail.com>
2018-12-28 16:36:15 +0100

    Change the main structure

--- src/sql/users-system.sql
+++ src/sql/users-system.sql
@@ -4,16 +4,6 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `groups`
---
 
 CREATE TABLE `groups` (
   `group_id` int(11) NOT NULL,
@@ -59,6 +49,10 @@ CREATE TABLE `users_group` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
 --
+-- Indexes for dumped tables
+--
+
+--
 -- Indexes for table `groups`
 --
 ALTER TABLE `groups`
@@ -83,6 +77,10 @@ ALTER TABLE `users_group`
   ADD PRIMARY KEY (`id_users`,`id_group`);
 
 --
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
@@ -94,7 +92,3 @@ ALTER TABLE `groups`
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
